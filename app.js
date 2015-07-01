27ea970d81b039bd628b82db167ce1ea426764c4
Collin Arnold <collin@collinarnold.net>
2015-06-30 18:16:25 -0700

    applied code diff before syntax highlighting

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 
 function loadDiff(leftText, rightText) {
-	var codeEl = document.getElementsByTagName('code')[0];
-	codeEl.innerText = rightText;
+	var codeEl = document.getElementsByTagName('code')[0],
+		diff = JsDiff.diffChars(leftText, rightText);
+		
+	codeEl.innerHTML = diff.map(function (part) {
+		var value = part.value;
+		if (value === '\r\n' || value === '\n') {
+			value = ' \n';
+		}
+		
+		return part.added ? ('<span class="diff-added">' + value + '</span>')
+			: part.removed ? ('<span class="diff-removed">' + value + '</span>')
+			: part.value;
+	}).join('');
 	hljs.highlightBlock(codeEl);
 }
 

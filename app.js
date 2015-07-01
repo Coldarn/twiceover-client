801dff29d08e40f92eaa4cee5cc52f66e92b4397
Collin Arnold <collin.arnold@gmail.com>
2015-06-30 21:20:12 -0700

    replacing tabs with spaces

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 
 function loadDiff(leftText, rightText) {
-	var codeEl = document.getElementsByTagName('code')[0],
-		diff = JsDiff.diffChars(leftText, rightText);
-		
-	codeEl.innerHTML = diff.map(function (part) {
-		var value = part.value;
-		if (value === '\r\n' || value === '\n') {
-			value = ' \n';
-		}
-		
-		return part.added ? ('<span class="diff-added">' + value + '</span>')
-			: part.removed ? ('<span class="diff-removed">' + value + '</span>')
-			: part.value;
-	}).join('');
-	hljs.highlightBlock(codeEl);
+    var codeEl = document.getElementsByTagName('code')[0],
+        diff = JsDiff.diffChars(leftText, rightText);
+
+    codeEl.innerHTML = diff.map(function (part) {
+        var value = part.value;
+        if (value === '\r\n' || value === '\n') {
+            value = ' \n';
+        }
+
+        return part.added ? ('<span class="diff-added">' + value + '</span>')
+            : part.removed ? ('<span class="diff-removed">' + value + '</span>')
+            : part.value;
+    }).join('');
+    hljs.highlightBlock(codeEl);
 }
 
 function loadFiles(leftFile, rightFile) {
-	var fs = require('fs');
-	fs.readFile(leftFile, function (err, leftData) {
-		if (err) {
-			throw err;
-		}
-		
-		fs.readFile(rightFile, function (err, rightData) {
-			if (err) {
-				throw err;
-			}
-			loadDiff(leftData.toString(), rightData.toString());
-		});
-	});
+    var fs = require('fs');
+    fs.readFile(leftFile, function (err, leftData) {
+        if (err) {
+            throw err;
+        }
+
+        fs.readFile(rightFile, function (err, rightData) {
+            if (err) {
+                throw err;
+            }
+            loadDiff(leftData.toString(), rightData.toString());
+        });
+    });
 }
 
 hljs.configure({
-	tabReplace: '    ', // 4 spaces
+    tabReplace: '    ', // 4 spaces
 });
 
 loadFiles('test/left.js', 'test/right.js');

68f7680d77f6b8e8fd6acf9d27028525973820cb
Collin Arnold <collin.arnold@gmail.com>
2015-07-05 14:26:49 -0700

    fleshed out the review creation UI

--- ui/ImportDialog.js
+++ ui/ImportDialog.js
@@ -1,6 +1,26 @@
 define(function () {
     'use strict';
     
+    function handleKeydown(event) {
+        if (event.keyCode === 13) {
+            handleAddReviewer();
+        }
+    }
+    
+    function handleAddReviewer() {
+        var entryEl = document.getElementById('review-nameentry'),
+            reviewerListEl = document.getElementById('revier-container');
+        
+        if (!entryEl.value || entryEl.value.length < 2) {
+            return;
+        }
+        
+        reviewerListEl.appendChild(
+            document.createRange().createContextualFragment(`<div>${entryEl.value}</div>`));
+        reviewerListEl.style.display = null;
+        entryEl.value = '';
+    }
+    
     var ImportDialog = {
         show: function (newIteration) {
             requirejs(['text!partials/ImportDialog.html'], function (html) {
@@ -15,6 +35,8 @@ define(function () {
                     el.querySelector('button.close').addEventListener('click', function () {
                         ImportDialog.hide();
                     });
+                    document.getElementById('add-reviewer').addEventListener('click', handleAddReviewer);
+                    document.getElementById('review-nameentry').addEventListener('keydown', handleKeydown);
                 }
                 
                 el.querySelector('button.close').style.display = newIteration ? null : 'none';

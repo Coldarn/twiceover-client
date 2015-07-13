6ad9dc820077a1757f7f7e090d58029bcb2c03d5
Collin Arnold <collin.arnold@gmail.com>
2015-07-12 19:56:56 -0700

    removing the dev toolbar, use F9 to reload and F12 to open dev tools

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ requirejs.config({
 
 requirejs([
     'App',
-    'util/Util',
+    'util/ElementProxy',
     'ui/MenuBar',
     'ui/FileList',
     'ui/CodeViewer',
     'ui/ImportDialog'
-], function (App, Util, MenuBar, FileList, CodeViewer, ImportDialog) {
+], function (App, ElementProxy, MenuBar, FileList, CodeViewer, ImportDialog) {
     'use strict';
 
     hljs.configure({
         tabReplace: '    ', // 4 spaces
+    });
+    
+    var gui = require('nw.gui');
+    
+    ElementProxy(document.body).on('keydown', function (event) {
+        if (event.keyCode === 120) {        // F9
+            gui.Window.get().reloadDev();
+        } else if (event.keyCode === 123) { // F12
+            gui.Window.get().showDevTools();
+        }
     });
 
     ImportDialog()

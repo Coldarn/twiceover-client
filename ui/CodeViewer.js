2b51c13a00e31841d99da4bc13ec99f7bceb9a19
Collin Arnold <collin.arnold@gmail.com>
2015-07-16 00:04:27 -0700

    further implementation of line highlighting
    it's still not done, but getting closer. I really dislike the candy cane
    effect when there are both adds and deletes on the same line, but don't
    have anything better yet! Blar.

--- ui/CodeViewer.js
+++ ui/CodeViewer.js
@@ -89,14 +89,12 @@ define([
     
     const workQueue = WorkQueue();
     
-    function highlightDifference(diff, line) {
-        return diff.added ? `<span class="diff-added">${line}</span>`
-            : diff.removed ? `<span class="diff-removed">${line}</span>`
-            : line;
-    }
-    
     const self = {
         __proto__: Component.prototype,
+        
+        initComponent: function () {
+            self.codeEl = self.el.querySelector('.code-block');
+        },
 
         loadActiveEntry: function () {
             const path = (App.leftEntry || App.rightEntry).path,
@@ -106,25 +104,33 @@ define([
             self.activeTaskName = taskName;
 
             if (task && task.diff) {
-                const content = task.diff
+                const highlightBlocks = [LineHighlight()];
+                
+                self.codeEl.innerHTML = '<code class="line-highlights"></code>' + task.diff
                     .filter(function (part) {
                         return !((App.diffMode === 'left' && part.added) || (App.diffMode === 'right' && part.removed));
                     })
                     .map(function (part) {
-                        if (App.diffMode === 'char') {
-                            return Util.escapeHtml(part.value)
-                                .replace(/\r\n/g, '\n')
-                                .split('\n')
-                                .map(highlightDifference.bind(null, part))
-                                .join('</div><div class="code-line">');
-                        } else {
-                            return highlightDifference(part, part.value);
+                        const value = Util.escapeHtml(part.value);
+                        let newlineCount = countLines(value);
+                    
+                        if (part.added || part.removed) {
+                            const lastHighlight = highlightBlocks[highlightBlocks.length - 1];
+                            lastHighlight.set(part.added, part.removed);
                         }
-                    }).join('');
+                        for (; newlineCount > 0; newlineCount -= 1) {
+                            highlightBlocks.push(LineHighlight(part.added, part.removed));
+                        }
+                    
+                        return part.added ? `<span class="diff-added">${value}</span>`
+                            : part.removed ? `<span class="diff-removed">${value}</span>`
+                            : value;
+                    }).join('').replace(/\n\n/g, '\n \n');
                 
-                self.el.innerHTML = App.diffMode === 'char' ? `<div class="code-line">${content}</div>` : content;
-                self.el.setAttribute('class', path.substring(path.lastIndexOf('.') + 1));
-                hljs.highlightBlock(self.el);
+                self.highlightsEl = self.el.querySelector('.line-highlights');
+                self.highlightsEl.innerHTML = highlightBlocks.join('');
+                self.codeEl.setAttribute('class', 'code-block ' + path.substring(path.lastIndexOf('.') + 1));
+                hljs.highlightBlock(self.codeEl);
             }
         },
 
@@ -160,7 +166,41 @@ define([
     EventBus.on('active_iterations_changed', self.handleIterationsChanged, self);
     EventBus.on('diff_completed', self.handleDiffCompleted, self);
     
-    self.setEl(document.querySelector('.code-pane > code'));
+    self.setEl(document.querySelector('.code-pane'));
+    
+    
+    
+    function countLines(str) {
+        let count = -1,
+            lastIndex = -1;
+        do {
+            count += 1
+            lastIndex = str.indexOf('\n', lastIndex + 1);
+        } while (lastIndex >= 0);
+        return count;
+    }
+    
+    const LineHighlight = (function () {
+        const proto = {
+            set: function (added, removed) {
+                this.added = added || this.added;
+                this.removed = removed || this.removed;
+            },
+            toString: function () {
+                if (this.added || this.removed) {
+                    return `<div class="code-line ${this.added ? 'added' : ''} ${this.removed ? 'removed' : ''}">\n</div>`;
+                }
+                return '\n';
+            }
+        };
+        
+        return function LineHighlight(added, removed) {
+            const obj = Object.create(proto);
+            obj.set(added, removed);
+            return obj;
+        };
+    }());
+    
                 
     return self;
 });

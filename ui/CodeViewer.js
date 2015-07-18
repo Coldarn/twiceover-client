b45aeef2bc572b062ff5754c2ee1c7dfa96b623d
Collin Arnold <collin.arnold@gmail.com>
2015-07-18 12:57:40 -0700

    factored out the background diff worker
    the new class is called DiffWorkQueue

--- ui/CodeViewer.js
+++ ui/CodeViewer.js
@@ -1,93 +1,13 @@
 define([
     'util/Util',
     'util/EventBus',
+    'util/DiffWorkQueue',
     'ui/Component',
     'App'
-], function (Util, EventBus, Component, App) {
+], function (Util, EventBus, DiffWorkQueue, Component, App) {
     'use strict';
     
-    function getTaskName(path, diffMode) {
-        return `${path}+${diffMode === 'char' ? 'char' : 'line'}`;
-    }
-    
-    function DiffTask(path, diffMode, leftContent, rightContent) {
-        return {
-            name: getTaskName(path, diffMode),
-            path: path,
-            diffMode: diffMode,
-            leftContent: leftContent,
-            rightContent: rightContent,
-            diff: null                  // Filled in when the background task completes
-        };
-    }
-    
-    function WorkQueue() {
-        const self = {
-            workers: [],
-            tasks: {},
-            queue: [],
-            
-            addTask: function (task) {
-                if (self.tasks[task.name]) {
-                    throw new Error("Task already enqueued!");
-                }
-                self.tasks[task.name] = task;
-                self.queue.push(task.name);
-                
-                if (self.workers.length < 4) {
-                    const worker = new Worker('util/BackgroundDiffer.js');
-                    worker.onmessage = self.handleWorkerMessage.bind(self, worker);
-                    self.workers.push(worker);
-                    self.sendWork(worker);
-                }
-            },
-            
-            getTask: function (name) {
-                return self.tasks[name];
-            },
-            
-            moveToFront: function (name) {
-                const index = self.queue.indexOf(name);
-                if (index >= 0) {
-                    self.queue.splice(index, 1);
-                    self.queue.unshift(name);
-                }
-            },
-            
-            handleWorkerMessage: function (worker, message) {
-                const task = self.tasks[message.data.name];
-                if (task) {
-                    task.diff = message.data.diff;
-                    EventBus.fire('diff_completed', task.path, task.diffMode);
-                } else {
-                    console.warn(`Received worker message for unknown task: ${message.data.name}`);
-                }
-                self.sendWork(worker);
-            },
-            
-            sendWork: function (worker) {
-                if (self.queue.length) {
-                    const taskName = self.queue.shift();
-                    worker.postMessage(self.tasks[taskName]);
-                } else {
-                    self.workers.splice(self.workers.indexOf(worker), 1);
-                    worker.terminate();
-                }
-            },
-            
-            cancelAll: function () {
-                self.workers.forEach(function (worker) {
-                    worker.terminate();
-                });
-                self.workers = [];
-                self.tasks = {};
-                self.queue = [];
-            }
-        };
-        return self;
-    }
-    
-    const workQueue = WorkQueue();
+    const workQueue = DiffWorkQueue();
     
     const self = {
         __proto__: Component.prototype,
@@ -98,7 +18,7 @@ define([
 
         loadActiveEntry: function () {
             const path = (App.leftEntry || App.rightEntry).path,
-                taskName = getTaskName(path, App.diffMode),
+                taskName = DiffWorkQueue.getTaskName(path, App.diffMode),
                 task = workQueue.getTask(taskName);
 
             self.activeTaskName = taskName;
@@ -151,13 +71,13 @@ define([
                 const leftContent = pair.left && pair.left.content || ''; 
                 const rightContent = pair.right && pair.right.content || ''; 
                 
-                workQueue.addTask(DiffTask(path, 'line', leftContent, rightContent));
-                workQueue.addTask(DiffTask(path, 'char', leftContent, rightContent));
+                workQueue.addTask(path, 'line', leftContent, rightContent);
+                workQueue.addTask(path, 'char', leftContent, rightContent);
             });
         },
         
         handleDiffCompleted: function (path, diffMode) {
-            if (getTaskName(path, diffMode) === self.activeTaskName) {
+            if (DiffWorkQueue.getTaskName(path, diffMode) === self.activeTaskName) {
                 self.loadActiveEntry();
             }
         }
---
+++ util/DiffWorkQueue.js
@@ -0,0 +1,89 @@
+define([
+    'util/EventBus'
+], function (EventBus) {
+    'use strict';
+    
+    function DiffTask(path, diffMode, leftContent, rightContent) {
+        return {
+            name: DiffWorkQueue.getTaskName(path, diffMode),
+            path: path,
+            diffMode: diffMode,
+            leftContent: leftContent,
+            rightContent: rightContent,
+            diff: null                  // Filled in when the background task completes
+        };
+    }
+    
+    function DiffWorkQueue() {
+        const self = {
+            workers: [],
+            tasks: {},
+            queue: [],
+            
+            addTask: function (path, diffMode, leftContent, rightContent) {
+                const task = DiffTask(path, diffMode, leftContent, rightContent);
+                if (self.tasks[task.name]) {
+                    throw new Error("Task already enqueued!");
+                }
+                self.tasks[task.name] = task;
+                self.queue.push(task.name);
+                
+                if (self.workers.length < 4) {
+                    const worker = new Worker('util/BackgroundDiffer.js');
+                    worker.onmessage = self.handleWorkerMessage.bind(self, worker);
+                    self.workers.push(worker);
+                    self.sendWork(worker);
+                }
+            },
+            
+            getTask: function (name) {
+                return self.tasks[name];
+            },
+            
+            moveToFront: function (name) {
+                const index = self.queue.indexOf(name);
+                if (index >= 0) {
+                    self.queue.splice(index, 1);
+                    self.queue.unshift(name);
+                }
+            },
+            
+            handleWorkerMessage: function (worker, message) {
+                const task = self.tasks[message.data.name];
+                if (task) {
+                    task.diff = message.data.diff;
+                    EventBus.fire('diff_completed', task.path, task.diffMode);
+                } else {
+                    console.warn(`Received worker message for unknown task: ${message.data.name}`);
+                }
+                self.sendWork(worker);
+            },
+            
+            sendWork: function (worker) {
+                if (self.queue.length) {
+                    const taskName = self.queue.shift();
+                    worker.postMessage(self.tasks[taskName]);
+                } else {
+                    self.workers.splice(self.workers.indexOf(worker), 1);
+                    worker.terminate();
+                }
+            },
+            
+            cancelAll: function () {
+                self.workers.forEach(function (worker) {
+                    worker.terminate();
+                });
+                self.workers = [];
+                self.tasks = {};
+                self.queue = [];
+            }
+        };
+        return self;
+    }
+    
+    DiffWorkQueue.getTaskName = function (path, diffMode) {
+        return `${path}+${diffMode === 'char' ? 'char' : 'line'}`;
+    };
+    
+    return DiffWorkQueue;
+});

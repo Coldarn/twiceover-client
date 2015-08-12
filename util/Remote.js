937f3b8603e21d39878f3177312977b39b2479dc
Collin Arnold <collin@collinarnold.net>
2015-08-11 21:18:56 -0700

    replaced WebSocket with ReconnectingWebSocket

--- util/Remote.js
+++ util/Remote.js
@@ -1,4 +1,6 @@
-define([], function () {
+define([
+    'util/Util'
+], function (Util) {
     'use strict';
     
     const proto = {
@@ -7,9 +9,24 @@ define([], function () {
     
     return function Remote(serverInfo) {
         const obj = Object.create(proto);
-        obj.webSocket = new WebSocket('ws://' + serverInfo.url);
+        obj.webSocket = new ReconnectingWebSocket('ws://' + serverInfo.url);
         obj.webSocket.onopen = function (event) {
             console.log('Server connected.');
+            
+            obj.webSocket.send(JSON.stringify({
+                type: 'time-sync',
+                clientSend: Util.highResTime()
+            }));
+        };
+        obj.webSocket.onmessage = function (evt) {
+            const message = JSON.parse(evt.data);
+            if (message.type === 'time-sync') {
+                const clientReceive = Util.highResTime();
+                console.log('Time offset:', ((message.serverReceive - message.clientSend) + (message.serverSend - clientReceive)) / 2);
+            }
+        };
+        obj.webSocket.onclose = function (event) {
+            console.log('Server connection lost.');
         };
     };
 });

802b61759a2ec5ba072da663eff28dfc6e62b678
Collin Arnold <collin@collinarnold.net>
2015-08-24 22:09:36 -0700

    the menu bar color now reflects the review's status

--- app/ui/MenuBar.js
+++ app/ui/MenuBar.js
@@ -6,7 +6,7 @@ define([
     'ui/ImportDialog'
 ], function (Component, EventBus, App, Home, ImportDialog) {
     'use strict';
-    
+
     var self = {
         __proto__: Component.prototype,
 
@@ -18,29 +18,30 @@ define([
 
             const addIteration = App.user.is(App.review.owningUser)
                 ? `<div class="iteration new-iteration">+</div>` : '';
-            
+
+            this.el.setAttribute('class', 'menu-bar ' + App.review.status);
             this.el.innerHTML = `<span class="home-button">Twice-Over</span> ${iterationHtml}${addIteration}`;
-            
+
             this.query('.home-button').on('click', this.handleHomeClick);
             this.queryAll('.iteration')
                 .on('click', this.handleIterationClick)
                 .on('contextmenu', this.handleIterationClick);
         },
-        
+
         handleHomeClick: function (e) {
             Home(true).appendTo(document.body);
         },
-        
+
         handleIterationClick: function (e) {
             e.preventDefault();
-            
+
             if (this.classList.contains('new-iteration')) {
                 ImportDialog().appendTo(document.body).whenLoaded(function (comp) { comp.show(true); });
             } else {
                 const clickedIndex = Number(this.dataset.index);
                 let leftIndex = App.leftIteration.index,
                     rightIndex = App.rightIteration.index;
-                
+
                 if (e.button !== 0 || e.ctrlKey) {
                     leftIndex = Math.min(clickedIndex, App.review.iterations.length - 2);
                     if (clickedIndex >= rightIndex) {
@@ -54,19 +55,24 @@ define([
                 }
                 App.setActiveIterations(leftIndex, rightIndex);
             }
-            
+
             return false;
         },
 
         handleActiveIterationsChanged: function() {
             this.populate();
+        },
+
+        handleReviewStatusChanged: function (event) {
+            this.populate();
         }
     };
-    
+
     EventBus.on('active_iterations_changed', self.handleActiveIterationsChanged, self);
     EventBus.on('review_iteration_added', self.handleActiveIterationsChanged, self);
-    
+    EventBus.on('change_review_status', self.handleReviewStatusChanged, self);
+
     self.setEl(document.querySelector('.menu-bar'));
-    
+
     return self;
 });

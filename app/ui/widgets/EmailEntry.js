162f58e36ad2f7d3bb0392ace43c9ea127e09566
Collin Arnold <collin@collinarnold.net>
2015-08-26 23:10:03 -0700

    fixed an escaping issue causing emails to be lost
    ...in the review email entry widget. Specifically, emails addresses
    wrapped in angle brackets (in the form "Name <foo@bar.com>") were eaten.

--- app/ui/widgets/EmailEntry.js
+++ app/ui/widgets/EmailEntry.js
@@ -1,21 +1,22 @@
 define([
+	'util/Util',
 	'ui/util/Component',
 	'ui/util/ElementProxy',
 	'util/EventBus',
     'integrations/EmailChecker'
-], function (Component, ElementProxy, EventBus, EmailChecker) {
+], function (Util, Component, ElementProxy, EventBus, EmailChecker) {
 	'use strict';
-	
+
 	const proto = {
 		__proto__:	Component.prototype,
-		
+
 		initComponent: function () {
 			var me = this;
-            
+
             me.entryEl = this.el.querySelector('#review-nameentry');
             me.suggestionsEl = me.el.querySelector('.email-suggestions');
             me.addButtonEl = me.el.querySelector('#add-reviewer');
-            
+
             ElementProxy(me.suggestionsEl).on('click', function (event) {
                 me.selectSuggestion(event.toElement.innerText);
             }).on('mouseover', function () {
@@ -30,61 +31,61 @@ define([
                 .on('keypress', me.handleEmailTextEntry.bind(me))
                 .on('keyup', me.handleEmailTextEntry.bind(me));
 		},
-        
+
         getEntries: function () {
             return this.queryAll('.email > span:first-child').map(function (el) {
                 return el.innerText.trim();
             });
         },
-        
+
         loadSuggestions: function () {
             const me = this,
                 checkValue = me.entryEl.value;
-            
+
             this.checkInputValid();
             if (me.pendingSuggestions || checkValue === me.lastCheckValue) {
                 return;
             }
-            
+
             me.lastCheckValue = checkValue;
             me.pendingSuggestions = EmailChecker.getSuggestions(checkValue).then(function (suggestions) {
                 me.pendingSuggestions = null;
-                
+
                 if (me.entryEl.value.length < 2) {
                     me.clearSuggestions();
                     return;
                 }
-                
+
                 suggestions = suggestions.slice(0, 20);
                 me.suggestionsEl.innerHTML = suggestions.map(function (email) {
                     return `<div>${email}</div>`;
                 }).join('');
                 me.suggestionsEl.style.display = null;
-                
+
                 if (checkValue !== me.entryEl.value) {
                     me.loadSuggestions();
                 }
             });
         },
-        
+
         clearSuggestions: function () {
             this.suggestionsEl.style.display = 'none';
             delete this.lastCheckValue;
         },
-        
+
         selectSuggestion: function (text) {
             this.entryEl.value = text;
             this.handleAddReviewer();
         },
-        
+
         checkInputValid: function () {
             const isValid = /[^@]+@[^@.]+\.[^@.]+/.test(this.entryEl.value);
             this.addButtonEl.classList.toggle('disabled', !isValid);
             return isValid;
         },
-        
-        
-		
+
+
+
         handleKeydown: function (event) {
             const focusedSuggestionEl = this.suggestionsEl.querySelector('.focus');
             switch (event.keyCode) {
@@ -115,13 +116,13 @@ define([
                     break;
             }
         },
-        
+
         handleEmailTextEntry: function (event) {
             if (this.entryEl.value.length > 1) {
                 this.loadSuggestions();
             } else {
                 this.clearSuggestions();
-            }  
+            }
         },
 
         handleAddReviewer: function() {
@@ -132,20 +133,20 @@ define([
                 return;
             }
 
-            const itemEl = Component(`<div class="email"><span>${me.entryEl.value}</span><span class="fa fa-times"></span></div>`);
+            const itemEl = Component(`<div class="email"><span>${Util.escapeHtml(me.entryEl.value)}</span><span class="fa fa-times"></span></div>`);
             itemEl.appendTo(reviewerListEl);
             itemEl.query('.fa').on('click', function () { itemEl.destroy(); });
-            
+
             reviewerListEl.style.display = null;
             me.entryEl.value = '';
             me.clearSuggestions();
             me.checkInputValid();
             me.entryEl.focus();
-			
+
 			EventBus.fire('reviewer_add_remove');
         }
     };
-	
+
 	return function EmailEntry() {
 		var obj = Object.create(proto);
         obj.setHtml('text!partials/EmailEntry.html');
